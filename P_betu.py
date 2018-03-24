8d0776c21d4152c21447ea8e63ff9c69fb99ff6c
Peter Neumark <neumark.peter@gmail.com>
2018-03-24 09:43:20 +0100

    asdf

---
+++ P_betu.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from magyarteknoc import *
+
+menj_előre(12)
+fordulj_jobbra()
+menj_előre(5)
+fordulj_jobbra()
+menj_előre()
+fordulj_balra()
+menj_előre()
+fordulj_jobbra()
+menj_előre(4)
+fordulj_jobbra()
+menj_előre()
+fordulj_balra()
+menj_előre()
+fordulj_jobbra()
+menj_előre(5)
--- magyarteknoc.py
+++ magyarteknoc.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+__all__ = ['menj_előre', 'fordulj_jobbra', 'fordulj_balra']
 
 from mineturtle import Turtle
 global t
 t = Turtle()
 t.angle(0)
+
+
 
 def menj_előre(hány_lépést=1):
     t.go(hány_lépést)
@@ -16,6 +19,3 @@ def fordulj_jobbra(hányszor=1):
 def fordulj_balra(hányszor=1):
     for i in range(0, hányszor):
         t.left(90)
-
-def parancsok():
-    return dir()
