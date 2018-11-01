92bde9a5d3f1cdc5b265e538412a4e5e1f6b2a89
Peter Neumark <neumark.peter@gmail.com>
2018-11-01 10:29:25 +0100

    negyzet: gorog minta lett belole

--- negyzet.py
+++ negyzet.py
@@ -5,11 +5,37 @@ from magyarteknoc import *
 
 rajzoló.nagyíts(6)
 
-for oldalhosszúság in range(1, 64, 4):
-    rajzoló.menj_előre(oldalhosszúság)
+
+def egy_ismetles():
+    rajzoló.menj_előre(1)
+    rajzoló.fordulj_balra()
+    rajzoló.menj_előre(4)
     rajzoló.fordulj_jobbra()
-    rajzoló.menj_előre(oldalhosszúság)
+    rajzoló.menj_előre(4)
     rajzoló.fordulj_jobbra()
-    rajzoló.menj_előre(oldalhosszúság)
+    rajzoló.menj_előre(3)
     rajzoló.fordulj_jobbra()
-    rajzoló.menj_előre(oldalhosszúság)
+    rajzoló.menj_előre(2)
+    rajzoló.fordulj_jobbra()
+    rajzoló.menj_előre(1)
+    rajzoló.fordulj_jobbra()
+    rajzoló.menj_előre(1)
+    rajzoló.fordulj_balra()
+    rajzoló.menj_előre(1)
+    rajzoló.fordulj_balra()
+    rajzoló.menj_előre(2)
+    rajzoló.fordulj_balra()
+    rajzoló.menj_előre(3)
+    rajzoló.fordulj_balra()
+    rajzoló.menj_előre(3)
+
+
+for oldalhossz in range(0,10):
+    for oldal in range(0, oldalhossz):
+        egy_ismetles()
+    rajzoló.fordulj_jobbra()
+
+
+while True:
+    pass
+
